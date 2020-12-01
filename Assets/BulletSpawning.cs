512481f6402d70d3327eb37003241b288b83b453
Coolp-GitHub <75265429+Coolp-GitHub@users.noreply.github.com>
2020-11-30 19:40:46 -0500

    ShapeWarsGitHub

---
+++ Assets/BulletSpawning.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawning : MonoBehaviour
+{
+    public Transform firepoint;
+    public GameObject bulletPrefab;
+
+    public float bulletForce = 20f;
+    public float coolDown = 0.2f;
+    bool canShoot = true;
+
+    void Update()
+    {
+        if ((Input.GetButtonDown("Fire1")) && canShoot)
+        {
+            Shoot();
+            StartCoroutine(WaitCooldown());
+
+        }
+        else
+        {
+            return;
+        }
+        
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firepoint.up * bulletForce, ForceMode2D.Impulse);
+        canShoot = false;
+        
+    }
+
+    IEnumerator WaitCooldown()
+    {
+        yield return new WaitForSeconds(coolDown);
+        canShoot = true;
+    }
+
+}
---
+++ Assets/MovementShooting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementShooting : MonoBehaviour
+{
+    public float speed = 5f;
+
+    public Camera cam;
+    public Rigidbody2D rb;
+
+    Vector2 move;
+    Vector2 mousePos;
+
+    void Update()
+    {
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void FixedUpdate()
+    {
+        rb.position = (rb.position + move * speed * Time.deltaTime);
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle; 
+    }
+}
