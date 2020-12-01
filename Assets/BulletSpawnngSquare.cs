ca658e7ef49157db4c2f4d068a9409b3b5e4f4ae
Coolp-GitHub <75265429+Coolp-GitHub@users.noreply.github.com>
2020-12-01 10:30:46 -0500

    Added Square Character

---
+++ Assets/BulletSpawnngSquare.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawnngSquare : MonoBehaviour
+{
+
+    public Transform firepoint;
+    public GameObject bulletPrefab;
+    public Transform firepoint2;
+
+    public float bulletForce = 20f;
+    public float coolDown = 0.2f;
+    bool canShoot = true;
+
+    void Update()
+    {
+        if ((Input.GetButtonDown("Fire1")) && canShoot)
+        {
+            Shoot();
+            StartCoroutine(WaitCooldown());
+
+        }
+        else
+        {
+            return;
+        }
+
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+        GameObject bullet2 = Instantiate(bulletPrefab, firepoint2.position, firepoint2.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        Rigidbody2D rb2 = bullet2.GetComponent<Rigidbody2D>();
+        rb.AddForce(firepoint.up * bulletForce, ForceMode2D.Impulse);
+        rb2.AddForce(firepoint2.up * bulletForce, ForceMode2D.Impulse);
+        canShoot = false;
+
+    }
+
+    IEnumerator WaitCooldown()
+    {
+        yield return new WaitForSeconds(coolDown);
+        canShoot = true;
+    }
+}
