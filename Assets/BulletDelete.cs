8cb5db888b42c5cd918d4b1e4c273be749f3860f
Coolp-GitHub <75265429+Coolp-GitHub@users.noreply.github.com>
2020-12-01 19:45:14 -0500

    Knockback fixes
    Bullets dont knock player back infinitly anymore

---
+++ Assets/BulletDelete.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDelete : MonoBehaviour
+{
+    void OnCollisionEnter (Collision ci)
+    {
+        Destroy(gameObject);
+    }
+}
