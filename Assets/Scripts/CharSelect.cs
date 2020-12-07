8f02e77f7980862ff62260735b392cc9c6fa3221
Coolp-GitHub <mohanparasinder@gmail.com>
2020-12-07 15:22:30 -0800

    CHaracter selection

--- Assets/Scripts/CharSelect.cs
+++ Assets/Scripts/CharSelect.cs
@@ -4,11 +4,75 @@ using UnityEngine;
 
 public class CharSelect : MonoBehaviour
 {
+    public int index = 0;
     public GameObject[] Characters;
+    public bool charSelected = false;
+    
+    
+    Vector2 Charv;
 
-    // Update is called once per frame
     void Update()
     {
+        for (int i = 0; i == Characters.Length; i++)
+        {
+            GameObject CharDE = Characters[i];
+            var script = CharDE.GetComponent<MovementShooting>();
+            script.enabled = false;
+            charSelected = false;
+            
+        }
         
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            index++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            index--;
+        }
+
+        if (index > Characters.Length )
+        {
+            index = 0;
+        }
+
+        if (index < 0)
+        {
+            index = Characters.Length;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            charSelected = true;
+        }
+        if (!charSelected)
+        {
+            GameObject character = Characters[index];
+            Transform chaPos = character.GetComponent<Transform>();
+            chaPos.position = chaPos.position - chaPos.position;
+        }
+        else
+        {
+            GameObject character = Characters[index];
+            var script = character.GetComponent<MovementShooting>();
+            script.enabled = true;
+
+            for (int i = 0; i == Characters.Length; i++)
+            {
+                GameObject CharMil = Characters[i];
+                Transform CharMilTran = CharMil.GetComponent<Transform>();
+                if (i != index)
+                {
+                    CharMilTran.position = CharMilTran.position + CharMilTran.position + CharMilTran.position;
+                }
+                
+               
+
+            }
+
+        }
+       
+            
     }
 }
